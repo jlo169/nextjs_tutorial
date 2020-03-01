20dfa965b8f2391d2416b9a14d74ed9468ff800a
Jonathan <kakashi169@gmail.com>
2020-03-01 06:58:07 +0000

    adding props or attributes to Link tag does not really do anything as Link is a wrapper component. Any attributes or props need to applied to children

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Index() {
   return (
     <div>
-      <Link href='/about'>
+      <Link href='/about' title='About Pug'>
         <a>About Page</a>
       </Link>
       <p>Hello Puggy.js</p>
