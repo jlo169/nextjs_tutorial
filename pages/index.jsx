9c6bfcd6135fab2b7bfd9145d7cbbcbcebde8f3c
Jonathan <kakashi169@gmail.com>
2020-03-04 05:01:17 +0000

    added inpage styling. this type of styling is in-scope, so no other pages nor children will be affected by this styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,86 @@
 
 import Link from 'next/link';
 import Layout from '../components/layout';
-import fetch from 'isomorphic-unfetch';
+// import fetch from 'isomorphic-unfetch';
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(show => (
-        <li key={show.id}>
-          <Link href='/p/[id]' as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-);
+function getPosts() {
+  return [
+    {id: 'hello-puggy', title: 'Hello Puggy'},
+    {id: 'learn-puggyjs', title: 'Learning Puggy.js is awesome'},
+    {id: 'deploy-puggyjs', title: 'Deploy pugs with ZEIT'}
+  ];
+};
 
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Puggy.js Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <li key={post.id}> 
+            <Link href={"/p/[id]"} as={`/p/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <style>
+        {`
+          h1,
+          a {
+            font-family: 'Arial';
+          }
 
-  console.log('Data fetched. Count: ', data.length);
+          ul {
+            padding: 0;
+          }
 
-  return {
-    shows: data.map(entry => entry.show)
-  };
+          li {
+            list-style: none;
+            margin: 5px 0;
+          }
+
+          a {
+            text-decoration: none;
+            color: blue;
+          }
+
+          a:hover {
+            opacity: 0.6;
+          }
+        `}
+      </style>
+    </Layout>
+  );
 }
 
-export default Index;
+// const Index = props => (
+//   <Layout>
+//     <h1>Batman TV Shows</h1>
+//     <ul>
+//       {props.shows.map(show => (
+//         <li key={show.id}>
+//           <Link href='/p/[id]' as={`/p/${show.id}`}>
+//             <a>{show.name}</a>
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   </Layout>
+// );
 
+// Index.getInitialProps = async function() {
+//   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//   const data = await res.json();
 
+//   console.log('Data fetched. Count: ', data.length);
 
+//   return {
+//     shows: data.map(entry => entry.show)
+//   };
+// }
 
+// export default Index;
 
 
 // const PostLink = props => (
@@ -43,16 +90,3 @@ export default Index;
 //     </Link>
 //   </li>
 // );
-
-// export default function Blog() {
-//   return (
-//     <Layout>
-//       <p>Puggy.js Blog</p>
-//       <ul>
-//         <PostLink id='Hello Next.js' />
-//         <PostLink id='Puggy.js is awesome' />
-//         <PostLink id='Deploy apps with Zeit' />
-//       </ul>
-//     </Layout>
-//   );
-// }
