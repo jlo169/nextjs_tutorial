6e642da16beea45a37046a2f37e75598e3942f33
Jonathan <kakashi169@gmail.com>
2020-03-05 06:04:28 +0000

    reseting what the /p/[i] leads to

--- pages/p/[id].jsx
+++ pages/p/[id].jsx
@@ -1,39 +1,42 @@
 
 import Layout from '../../components/layout';
-import fetch from 'isomorphic-unfetch';
+// import fetch from 'isomorphic-unfetch';
 
-const Post = props => (
-  <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
-  </Layout>
-)
 
-Post.getInitialProps = async function(context) {
-  console.log('context is', context);
-  const {id} = context.query;
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-  const show = await res.json();
+export default function Post() {
+  const router = useRouter();
 
-  console.log(`Fetched show: ${show.name}`);
-
-  return {show};
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <p>This is puggy content inside p folder</p>
+    </Layout>
+  )
 }
 
-export default Post;
 
 
 
 
 
-// export default function Post() {
-//   const router = useRouter();
 
-//   return (
-//     <Layout>
-//       <h1>{router.query.id}</h1>
-//       <p>This is puggy content inside p folder</p>
-//     </Layout>
-//   )
+// const Post = props => (
+//   <Layout>
+//     <h1>{props.show.name}</h1>
+//     <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+//     {props.show.image ? <img src={props.show.image.medium} /> : null}
+//   </Layout>
+// )
+
+// Post.getInitialProps = async function(context) {
+//   console.log('context is', context);
+//   const {id} = context.query;
+//   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+//   const show = await res.json();
+
+//   console.log(`Fetched show: ${show.name}`);
+
+//   return {show};
 // }
+
+// export default Post;
